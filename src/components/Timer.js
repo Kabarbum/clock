7e3da02b50d6c1c0a28dbd736db65aafbf7b3abc
Kabarbum <misha.pro2856@gmail.com>
2023-11-08 21:32:37 +0500

    test inputs

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -187,7 +187,7 @@ const Timer = () => {
               <input type="text"/>text
               <input type="search"/>search
               
-              <input type="search" inputMode='search'/>inputModesearch
+              <input type="search" inputMode='search'/>inputModesearch1
           </div>
           </>
     );
